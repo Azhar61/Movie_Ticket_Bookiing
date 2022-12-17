f302d21d77bbf747663ff29e1cac22effe1f5168
Azhar Ali <110254456+Azhar61@users.noreply.github.com>
2022-12-17 07:24:04 +0500

    2nd_Commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,7 @@
 <body style="font-family: Poppins ;">
 <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top " style="font-family: Poppins ; line-height: 30px; ">
-        <a class="navbar-brand text-light "   href="#">BookiT</a>
+        <a class="navbar-brand text-light "   href="home">BookiT</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
